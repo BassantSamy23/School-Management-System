87e4227b39366bc2e6697a2d45d831f926646127
Bassant Samy <167112462+BassantSamy23@users.noreply.github.com>
2024-05-31 18:37:00 +0300

    Create STD_TYPES.h

---
+++ STD_TYPES.h
@@ -0,0 +1,11 @@
+Updates to keyboard shortcuts … On Thursday, August 1, 2024, Drive keyboard shortcuts will be updated to give you first-letters navigation.Learn more
+typedef unsigned char      		u8 ;
+typedef signed char        		s8 ;
+typedef unsigned short int 		u16;
+typedef signed short int   		s16;
+typedef unsigned long int  		u32;
+typedef signed long int         s32;
+typedef unsigned long long int  u64;
+typedef signed long long int    s64;
+typedef float                   f32;
+typedef double                  f64;
